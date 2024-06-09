3dc4315b3ba99d7f4feda795198a518c0df9f574
m0nt4ld0 <mmontaldo@live.com>
2024-06-09 13:11:08 -0300

    Soluciona bugs al operar con valores de mas de una cifra

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -1,4 +1,28 @@
+/*
+* ========================================================================================
+*  Nombre del archivo: calculadora.js
+*  Autor: Mariela Montaldo
+*  Fecha de creación: 07/06/2024
+*  Última modificación: 09/06/2024
+*  Versión: v0.1
+*
+*  Descripción:
+*  Sencilla calculadora realizada con Javascript.
+*
+*  Historial de modificaciones:
+*  - 07/06/2024: Mariela Montaldo - Creación del archivo.
+*
+*  Copyright (c) 2024 Mariela Montaldo.
+*
+*  Licencia:
+*  Este código está licenciado bajo la GPL-2.0. Para más información, 
+*  consulte el archivo LICENSE adjunto en el directorio raíz del proyecto o visite 
+*  https://fsf.org/.
+*
+* ========================================================================================
+*/
 
+/* ===== Definiciones de variables y constantes ===== */
 const VALORES = document.querySelectorAll('.btn-valor');
 const OPERACIONES = document.querySelectorAll('.btn-operacion');
 const VISOR = document.getElementById('visor');
@@ -7,37 +31,71 @@ let gFlag = 0;
 let gNumbers = [0, 0];
 let gOperacion = '';
 
+/* ===== Eventos ===== */
+
 VALORES.forEach(valor => {
     valor.addEventListener('click', () => {
-        gNumbers[gFlag] += parseInt(valor.value);
-        VISOR.value = '';
-        VISOR.value = gNumbers[gFlag] ;
+        VISOR.value += valor.value;
+        gNumbers[gFlag] = parseInt(VISOR.value);
     });
 });
 
 OPERACIONES.forEach(operacion => {
     operacion.addEventListener('click', () => {
         VISOR.value = '';
-        if(operacion.value == "=" || gFlag > 0) {
-            gFlag = 0;
+        if(operacion.value == "=") {
             calcular();
+            gFlag = 0;
         }
         if(operacion.value != ",") {
-            gFlag++;
+            gFlag = 1;
             gOperacion = operacion.value;
+        }
+        if(operacion.value == "AC") {
+            limpiar();
+        }
+        if(operacion.value == "+/-") {
+            cambiarSigno();
         }
     });
 });
 
+/* ===== Funciones ===== */
+
 function calcular() {
+    let rdo = 0;
     switch(gOperacion) {
         case "+":
-            VISOR.value = parseInt(gNumbers[0]) + parseInt(gNumbers[1]);
+            rdo = (gNumbers[0] + gNumbers[1]);
+            break;
+        case "x":
+            rdo = (gNumbers[0] * gNumbers[1]);
             break;
         case "-":
-            VISOR.value = gNumbers[0] - gNumbers[1];
+            rdo = (gNumbers[0] - gNumbers[1]);
             break;
+        case "/":
+            rdo = (gNumbers[0] / gNumbers[1]);
+        break;
+        
     }
-    gNumbers = [parseInt(VISOR.value), 0];
+    VISOR.value = rdo;
+    gNumbers = [rdo, 0];
     gFlag = 1;
+}
+
+function limpiar() {
+    VISOR.value='';
+    gNumbers = [0, 0];
+    gFlag = 0;
+    gOperacion = '';
+}
+
+function cambiarSigno() {
+    //console.log("VISOR.value: " + VISOR.value+ " ,gNumbers[0] " + gNumbers[0] + ", gNumbers[1] " + gNumbers[1]);
+    let aux = parseInt(VISOR.value);
+    //console.log("aux antes: " + aux);
+    aux*=(-1);
+    //console.log("aux desp: " + aux);
+    VISOR.value=aux;
 }
